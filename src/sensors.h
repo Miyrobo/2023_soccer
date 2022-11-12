734a46bdb5d642726ddda5c48a57f044282a739d
{YMiya-robo} <{my1yamato@gmail.com}>
2022-11-12 17:35:32 +0900

    ver.2

--- src/sensors.h
+++ src/sensors.h
@@ -1,15 +1,23 @@
+#include "Arduino.h"
+
 #define NUM_balls 16
 #define NUM_lines 25
+
+double SIN16[16]={0.0,0.383,0.707,0.924,1.0,0.924,0.707,0.383,0,-0.383,-0.707,-0.924,-1.0,-0.924,-0.707,-0.383};
 
 class BALL{
     public:
         int value[NUM_balls];
-        int get();
+        void get();
+        int dir;         //ボールの方向
+        double distance;   //ボールまでの距離
+        int x,y;
     private:
-        int pin[NUM_balls]={};
+        int pin[NUM_balls]={};   //ピン番号
 };
 
 class LINE{
     public:
         int value[NUM_lines];
 };
+
